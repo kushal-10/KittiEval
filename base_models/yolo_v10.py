c1fdd2782c4f9a064d17d6f71d2e504991d58e1e
kushal-10 <kushalkoshti11@gmail.com>
2024-07-30 15:11:16 +0200

    change to HF import

--- base_models/yolo_v10.py
+++ base_models/yolo_v10.py
@@ -1,17 +1,5 @@
-# Load model directly
-from transformers import AutoModel
-model = AutoModel.from_pretrained("jameslahm/yolov10x")
+from ultralytics import YOLOv10
 
-
-# # Run batched inference on a list of images
-# results = model(["im1.jpg", "im2.jpg"])  # return a list of Results objects
-#
-# # Process results list
-# for result in results:
-#     boxes = result.boxes  # Boxes object for bounding box outputs
-#     masks = result.masks  # Masks object for segmentation masks outputs
-#     keypoints = result.keypoints  # Keypoints object for pose outputs
-#     probs = result.probs  # Probs object for classification outputs
-#     obb = result.obb  # Oriented boxes object for OBB outputs
-#     result.show()  # display to screen
-#     result.save(filename="result.jpg")  # save to disk
+model = YOLOv10.from_pretrained('jameslahm/yolov10x')
+source = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+model.predict(source=source, save=True)
