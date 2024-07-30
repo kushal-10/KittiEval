ac301eb991a70920fb3ae3bc165d9b3b758fb27c
kushal-10 <kushalkoshti11@gmail.com>
2024-07-30 18:13:17 +0200

    cleanup

--- base_models/yolo_v10.py
+++ base_models/yolo_v10.py
@@ -1,5 +1,11 @@
 from ultralytics import YOLOv10
 
-model = YOLOv10.from_pretrained('jameslahm/yolov10x')
-source = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-model.predict(source=source, save=True)
+model = YOLOv10.from_pretrained('jameslahm/yolov10n')
+
+# Run batched inference on a list of images
+results = model(["data/images/000000.png", "data/images/000036.png"])  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes.xyxy  # Boxes object for bounding box outputs
+    print(boxes)
