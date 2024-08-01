14c0cc76511344fe269c050c8d32b8e231b59127
kushal-10 <kushalkoshti11@gmail.com>
2024-08-01 19:26:47 +0200

    add dataset downloader script form HF

---
+++ training/dataset/create_yolo_dataset.py
@@ -0,0 +1,6 @@
+# File to convert the data-set into YOLO compatible format,
+# as mentioned here - https://docs.ultralytics.com/datasets/detect/
+
+from datasets import load_dataset
+
+ds = load_dataset("Koshti10/omegalol1")
