2e25c2124f9aaa14abb4e305262f5809c6d72623
kushal-10 <kushalkoshti11@gmail.com>
2024-08-02 13:18:37 +0200

    test example

--- training/dataset/create_yolo_dataset.py
+++ training/dataset/create_yolo_dataset.py
@@ -18,19 +18,20 @@ counter = 0
 # Iterate through the dataset
 for idx, example in enumerate(dataset):
     # Replace 'image' with the key that holds the image data
-    image_url = example['image']
-
-    # Download the image
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
-
-    # Define the path to save the image
-    image_path = os.path.join(output_folder, f'image_{idx}.png')
-
-    # Save the image
-    img.save(image_path)
-
-    print(f'Saved image {idx} to {image_path}')
+    print(example)
+    # image_url = example['image']
+    #
+    # # Download the image
+    # response = requests.get(image_url)
+    # img = Image.open(BytesIO(response.content))
+    #
+    # # Define the path to save the image
+    # image_path = os.path.join(output_folder, f'image_{idx}.png')
+    #
+    # # Save the image
+    # img.save(image_path)
+    #
+    # print(f'Saved image {idx} to {image_path}')
 
     counter += 1
     if counter == 20:
