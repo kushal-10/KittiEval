e8d7d37c5c2cb35921fb6550f50842592890544d
kushal-10 <kushalkoshti11@gmail.com>
2024-08-05 19:24:13 +0200

    del prev token

--- yolo_training/train.py
+++ yolo_training/train.py
@@ -10,4 +10,5 @@ dataset_path = os.path.join('data', 'huggingface', 'data.yaml')
 results = model.train(data=dataset_path, epochs=500, batch=256, imgsz=640)
 
 # Save to HF
-model.push_to_hub("Koshti10/yolov10-trained-Kitti-2D-detection", token="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")
+HF_TOKEN = os.getenv('HF_TOKEN')
+model.push_to_hub("Koshti10/yolov10-trained-Kitti-2D-detection", token=HF_TOKEN)
