1fd022a5e6c6f9fb5c8c922be7ba23fb70877b0a
kushal-10 <kushalkoshti11@gmail.com>
2024-08-06 03:12:08 +0200

    fix args

--- yolo_training/train.py
+++ yolo_training/train.py
@@ -9,8 +9,8 @@ model = YOLOv10("yolov10x.yaml")
 
 # Train the model
 dataset_path = os.path.join('data.yaml')
-results = model.train(data=dataset_path, epochs=100, batch=4, imgsz=640, num_workers=4,
-                      save=True, device=0, cache=True, project='trained_models',
+results = model.train(data=dataset_path, epochs=100, batch=64, imgsz=640, workers=8,
+                      save=True, device=[0,1,2,3], cache=True, project='trained_models',
                       name='yolo10n_extreme_vanilla_a100', pretrained=False, plots=True)
 
 # Save to HF
